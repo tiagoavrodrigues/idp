5ed8682d61e166976cf46eb69918b1cd3c6f8bab
Tiago Rodrigues <tiagoalexandre@ipvc.pt>
2026-03-17 05:03:34 +0000

    mega: commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -12,4 +12,41 @@ export class App {
   protected readonly title = signal('idp');
 }
 
-console.log("Chave API:", environment.API_KEY)
+async function loadUsers() {
+  const response = await fetch(environment.apiUrl + '/users');
+  const users = await response.json();
+  return users;
+}
+
+async function login(user: { name: string; password: string }) {
+  const users = await loadUsers();
+
+  const foundUser = users.find(
+    (u: { name: string; password: string }) =>
+      u.name === user.name && u.password === user.password
+  );
+
+  if (foundUser) {
+    console.log("User válido");
+  } else {
+    console.log("User inválido");
+  }
+}
+
+login({
+  name: "Joaquim",
+  password: "123"
+});
+
+// const response = await fetch(environment.apiUrl + '/users', {
+//   method: "POST",
+//   headers: {
+//     "Content-Type": "application/json"
+//   },
+//   body: JSON.stringify(user)
+// });
+
+// const data = await response.json();
+// console.log(data);
+
+// createUser();
